1dba181cf6065e582e2ac62abcbf603cd0d36671
SnowMoto <motogirlmonday@gmail.com>
2025-02-08 13:55:06 -0700

    update routes

--- route/user_profile.js
+++ route/user_profile.js
@@ -9,8 +9,9 @@ router.get('/:id', userController.getSingle);
 
 router.post('/', userController.createUser);
 
+//Next Week
 router.put('/:id', userController.updateUser);
-
+//Next Week
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
